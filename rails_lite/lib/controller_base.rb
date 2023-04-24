6b204997e0a4db479e20f29cc8fc41fa9b28358f
treypisano <treypisano@treypc>
2023-04-24 12:45:21 -0400

    finish part_2

--- rails_lite/lib/controller_base.rb
+++ rails_lite/lib/controller_base.rb
@@ -2,26 +2,39 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require "byebug"
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.status = 302
+    @res["Location"] = url
+    raise "error" if already_built_response?
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res["Content-Type"] = content_type
+    # @res["Body"] = content
+    @res.write(content)
+    raise "error" if already_built_response?
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
